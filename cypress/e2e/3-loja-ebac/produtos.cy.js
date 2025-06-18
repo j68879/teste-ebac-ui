4a53db76732327cbbb338c2b421382bd57797d31
Jackson N. Freitas <jack960879@gmail.com>
2025-06-17 22:14:19 -0300

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,16 +1,30 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarURL()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-            .contains('Frankie Sweatshirt')
-            .click()
-        cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+            cy.get('#tab-title-description > a').should('contain', 'Descrição')
+        
+    });
+
+    it.only('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+        
+    });
+
+    it('Deve visitar a pagina do produto', () => {
+        
+    });
+    
+    it('Deve adicionar produto ao carrinho', () => {
         
     });
 });
---
+++ cypress/support/page-objects/produtos.page.js
@@ -0,0 +1,20 @@
+class ProdutosPage{
+
+    visitarURL(){
+        cy.visit('produtos')
+    }
+
+    buscarProduto(nomeProduto){
+        cy.get('[name=s]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
+    }
+
+    buscarProdutoLista(nomeProduto){
+         cy.get('.products > .row')
+            .contains(nomeProduto)
+            .click()
+        
+    }
+}
+
+export default new ProdutosPage()
